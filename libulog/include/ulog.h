fcba49995322a06b03a0c0edb4a7f9fa6de912c0
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2018-02-19 13:53:50 +0100

    [DEV] Add ULOG_EVT macro to log system events
    See https://smet.parrot.biz/projects/anafi/wiki/SystemEvent for more info
    
    
    
    
    (cherry picked from commit a7784ed544a7bc2a9224bdc012de4e2ee2e1cab4)

--- libulog/include/ulog.h
+++ libulog/include/ulog.h
@@ -170,6 +170,22 @@
 		      strerror(_err))
 
 /**
+ * Log as NOTICE an event log of the form
+ * EVT:<type>;<param1>=<value1>;<param2>=<value2>...
+ * <type>:   Type of the event. In upper case with '_' to separate words
+ *           (So a valid C enum identifier).
+ * <paramN>:   Name of parameter N: In lower case with '_' to separate words
+ *           (So a valid C variable identifier).
+ * <valueN>: Value of parameter N: in Quotes for string.
+ *           Avoid putting ';' in them or escape them with '\'.
+ *
+ * _type : Type of the event as a string
+ * _fmt : formatting string for arguments, pass empty string if no arguments
+ */
+#define ULOG_EVT(_type, _fmt, ...) \
+	ULOGN("EVT:" _type ";" _fmt, ##__VA_ARGS__)
+
+/**
  * Maximum length of an ascii message.
  *
  * Non-binary messages exceeding this length will be truncated.
