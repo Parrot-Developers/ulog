af496a96bdaccc886aaaad372d711b34381f4575
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2019-01-30 10:22:29 +0100

    [DEV] ulog_shell_api: add possibility to not redirect everything to ulog.
    ULOG_NO_REDIRECT_STD=y : do not redirect stderr and stdout to ulog.
    ULOG_STDERR=y : ulogX also log to stderr (implies ULOG_NO_REDIRECT_STD=y)

--- libulog/ulog_api.sh
+++ libulog/ulog_api.sh
@@ -4,9 +4,23 @@
 # to use it, on top of your shell script, do :
 #    ulog_tag="my super tag" # optional, defaulting to "(shell script)"
 #    source /usr/share/ulog/ulog_api.sh
+#
+# By default the script will redirect stdout and stderr to ulog.
+# Nothing will be printed to the console.
+# If you do not want this behaviour, set ULOG_NO_REDIRECT_STD=y
+# If you also want to see ulogX messages on the console, set ULOG_STDERR=y
+# This implies ULOG_NO_REDIRECT_STD=y
 
-# redirect stdout and stderr to ulog, to catch logs not issued with ulogX funcs
-exec 1> /dev/ulog_main 2>&1
+if [ "$ULOG_STDERR" = "y" ]
+then
+    ULOG_NO_REDIRECT_STD=y
+fi
+
+if [ "$ULOG_NO_REDIRECT_STD" != "y" ]
+then
+    # redirect stdout and stderr to ulog, to catch logs not issued with ulogX funcs
+    exec 1> /dev/ulog_main 2>&1
+fi
 
 ULOG_CRIT=2
 ULOG_ERR=3
@@ -14,6 +28,8 @@ ULOG_WARN=4
 ULOG_NOTICE=5
 ULOG_INFO=6
 ULOG_DEBUG=7
+
+ULOG_LEVEL_LETTERS="  CEWNID"
 
 ulog() {
 	level="$1"
@@ -23,7 +39,12 @@ ulog() {
 		ulog_tag="(shell script)"
 	fi
 
-	printf "%d\0\0\0%s\0%s\n" "${level}" "${ulog_tag}" "${message}"
+	if [ "$ULOG_STDERR" = "y" ]
+	then
+		printf "${ULOG_LEVEL_LETTERS:$level:1} ${message}\n" >&2
+	fi
+
+	printf "%d\0\0\0%s\0%s\n" "${level}" "${ulog_tag}" "${message}" > /dev/ulog_main
 }
 
 ulogc() {
@@ -49,4 +70,3 @@ ulogi() {
 ulogd() {
 	ulog ${ULOG_DEBUG} "$1"
 }
-
